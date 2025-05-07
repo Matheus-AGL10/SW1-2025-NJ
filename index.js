26cd1809557dd6fac85dfa3b7a78b947fcfb270f
Matheus-AGL10 <Matheus.etec2025@gmail.com>
2025-05-07 17:49:56 +0000

    Aula 07/05/2025

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ const path = require('path');
 const app = express();
 
 app.set('view engine', 'hbs');
-
-
 app.set('views', './views');
-
-
 app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-    res.render('home'); 
+    res.render('home', { nome: "Neymar Junior" });
+});
+
+
+app.get('/tabuada', (req, res) => {
+    let resultado = [];
+    for (let cont = 1; cont <= 10; cont++) {
+        resultado.push (cont * 5);
+    }
+    res.render ('tabuada' , {valores : resultado});
 });
 
 app.listen(3000, () => {
